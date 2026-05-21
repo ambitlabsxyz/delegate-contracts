a92ebf43d2d3eafc5037b1eae30a716dc24f2f30
cosullivan <cain.osullivan@gmail.com>
2026-05-21 15:10:27 +0800

    WIP

---
+++ script/Deploy.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import { Script } from "forge-std/Script.sol";
+import { stdJson } from "forge-std/StdJson.sol";
+import { console } from "forge-std/console.sol";
+import { Delegate } from "../src/Delegate.sol";
+
+// 0x3fBc32E2b50300e1b72f2206d1f23666bDF2176C
+
+contract Deploy is Script {
+  // forge script .\script\Deploy.s.sol:Deploy --rpc-url hyperevm --broadcast --account deployer --skip-simulation
+  function run() external {
+    vm.startBroadcast();
+
+    Delegate delegate = new Delegate();
+    console.log("Delegate deployed to:", address(delegate));
+
+    vm.stopBroadcast();
+  }
+}
