b7797384a781296b8bd0656ac7b740f211edd3b3
cosullivan <cain.osullivan@gmail.com>
2026-06-22 17:19:48 +0800

    updated deployment script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -8,12 +8,16 @@ import { Delegate } from "../src/Delegate.sol";
 
 // 0x3fBc32E2b50300e1b72f2206d1f23666bDF2176C
 
+// delegate-contracts.v1: 0x9f111F06ec7cAB13c4B757d253c66c658e008cc7
+
 contract Deploy is Script {
   // forge script .\script\Deploy.s.sol:Deploy --rpc-url hyperevm --broadcast --account deployer --skip-simulation
   function run() external {
     vm.startBroadcast();
 
-    Delegate delegate = new Delegate();
+    bytes32 salt = keccak256("delegate-contracts.v1");
+
+    Delegate delegate = new Delegate{ salt: salt }();
     console.log("Delegate deployed to:", address(delegate));
 
     vm.stopBroadcast();
