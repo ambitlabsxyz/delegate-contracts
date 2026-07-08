51d5521ffc44c0c7dc8f080e05b03a734d6d3fd1
cosullivan <cain.osullivan@gmail.com>
2026-07-08 17:24:35 +0800

    update the deployment

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -9,6 +9,7 @@ import { Delegate } from "../src/Delegate.sol";
 // 0x3fBc32E2b50300e1b72f2206d1f23666bDF2176C
 
 // delegate-contracts.v1: 0xb85fe8D2c4eFFf7AD8E76F9Be80Bd2049F637DBe
+// delegate-contracts.v2: 0xF1c8F785542E398F52A63f9B27984ce57fF03942
 
 address constant DEPLOYER = 0x6B887caC2e0Ef29306E1B6F22E716796105137a0;
 
@@ -23,7 +24,7 @@ contract Deploy is Script {
 
     ICreateX createX = ICreateX(0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed);
 
-    bytes32 salt = bytes32(abi.encodePacked(DEPLOYER, hex"00", bytes11(keccak256("delegate-contracts.v1"))));
+    bytes32 salt = bytes32(abi.encodePacked(DEPLOYER, hex"00", bytes11(keccak256("delegate-contracts.v2"))));
 
     bytes memory initCode = abi.encodePacked(type(Delegate).creationCode);
 
